a5f605bd7d1c637a3120b60ff9f74f6e88bc2240
Bálintfy Péter <balintfy.peter@burg.lo.hu>
2025-04-11 10:40:59 +0200

    fetch

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Carousel from "./components/Carousel"
+import Cars from "./components/Cars"
+
+function App() {
+  return (
+    <>
+      <Carousel></Carousel>
+    </>
+  )
+}
+
+export default App
